8b886ab0483d0b95438ce95cefcf4d2adac20431
Jardoz <ozeroy@gmail.com>
2023-07-05 22:14:41 +0300

    feat: create store form(with react-hook-form, zod validation, zustand state manager and shadcn components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,23 @@
 "use client"
+import { useEffect } from "react";
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/useStoreModal";
+
 
 const SetupPage = () => {
+    const onOpen = useStoreModal(({ onOpen }) => onOpen)
+    const isOpen = useStoreModal(({ isOpen }) => isOpen)
+
+    useEffect(() => {
+        if(!isOpen) {
+            onOpen()
+        }
+    }, [isOpen, onOpen])
+
     return (
         <>
             <div className="px-6 py-4">
-                <Modal title="Test" description="Test Description" isOpen onClose={()=> {}}>
-                    Children
-                </Modal>
+                Root Page                              
             </div>
         </>
      
--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -1,19 +1,72 @@
-"use client"
+"use client";
 
-import { useStoreModal } from "@/hooks/useStoreModal"
-import { Modal } from "@/components/ui/modal"
+import * as s from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { Modal } from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = s.object({
+  name: s.string().min(1),
+});
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
-    
-    return (
-        <Modal
-            title="Create Store"
-            description="Add a new store to manage products and categories"
-            isOpen={storeModal.isOpen}
-            onClose={storeModal.onClose}
-        >
-            Future Create Store Form
-        </Modal>
-    )
-}
+  const storeModal = useStoreModal();
+
+  const form = useForm<s.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = async (values: s.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO Create Store
+  };
+
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-Commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
