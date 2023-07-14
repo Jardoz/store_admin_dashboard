b8839505cfcce86c8f3ee844f041600d48838df5
Jardoz <ozeroy@gmail.com>
2023-07-14 10:45:31 +0300

    feat: colors entities

--- components/mainNav.tsx
+++ components/mainNav.tsx
@@ -38,6 +38,11 @@ export function MainNav({
         label: "Sizes",
         active: pathname === `/${params.storeId}/sizes`,
       },
+      {
+        href: `/${params.storeId}/colors`,
+        label: "Colors",
+        active: pathname === `/${params.storeId}/colors`,
+      },
     ];
 
     return (
