c09f9af1323c08d0303ddec62aa981e5194b290b
Jardoz <ozeroy@gmail.com>
2023-07-22 22:11:59 +0300

    feat: order telegram notification

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,7 +1,9 @@
-import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
+import axios from "axios";
+
+const TELEGRAM_API_URL = 'https://api.telegram.org/'
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -18,16 +20,13 @@ export async function POST(
   { params }: { params: { storeId: string } }
 ) {
   const request = await req.json();
-  const { productIds, message, messenger, phone } = request;
-  // { productAmount, phone, telegram/viber, customMessage, address? }
+  const { productIds, message, messenger, phone, name } = request;
 
   if (!productIds || productIds.length === 0) {
     return new NextResponse("Product ids are required", { status: 400 });
   }
 
   try {
-    console.log(request);
-    
     
     const order = await prismadb.order.create({
       data: {
@@ -47,11 +46,31 @@ export async function POST(
         }
       }
     });
+
+    const products = await prismadb.product.findMany({
+      where: {
+        id: {
+          in: productIds,
+        },
+      },
+    });
+
+
+    const productNames = products.map((product) => product.name).join(", ")
     
     
     const successUrl = `cart?success=1`
+    const successMessage = `
+      You've got an order from ${name} \n
+      phone number: ${phone},\n
+      messenger: ${messenger},\n
+      products: ${productNames}
+    `;
     
-    
+     const telegramResponse = await axios.get(
+       `${TELEGRAM_API_URL}bot${process.env.TEELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${successMessage}`
+     );
+
     
     return NextResponse.json({ url: successUrl, order }, {
       headers: corsHeaders
